79d5ff2a5ee0f9c226ec2e7194e66d083313f8e8
Mehedi Hasan <mehedi.hasan@appscode.com>
2020-12-10 14:23:59 +0600

    testing

---
+++ testing/main.go
@@ -0,0 +1,10 @@
+package main
+
+//Add function
+//add two vars
+func Add(x, y int) int {
+	return x + y
+}
+func main() {
+
+}
---
+++ testing/main_test.go
@@ -0,0 +1,32 @@
+package main
+
+import "testing"
+
+func TestAdd(t *testing.T) {
+	type args struct {
+		x int
+		y int
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		// TODO: Add test cases.
+		{
+			name: "fsit",
+			args: args{
+				x: 1,
+				y: 1,
+			},
+			want: 2,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := Add(tt.args.x, tt.args.y); got != tt.want {
+				t.Errorf("Add() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
